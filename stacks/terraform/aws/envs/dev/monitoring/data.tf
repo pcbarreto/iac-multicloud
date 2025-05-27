7ce86b253f3f0324f1e8c99de3fe9a2651691382
Paulo Barreto <monstryker@outlook.com>
2025-05-27 18:02:24 -0300

    Teste pipe

--- stacks/terraform/aws/envs/dev/monitoring/data.tf
+++ stacks/terraform/aws/envs/dev/monitoring/data.tf
@@ -9,7 +9,7 @@ data "aws_eks_cluster" "eks" {
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
-    bucket = "poc-multi-cloud-tfstate"
+    bucket = "poc-multicloud-tfstate"
     key    = "env:/dev/eks/stacks-terraform.tfstate"
     region = "us-east-1"
   }
