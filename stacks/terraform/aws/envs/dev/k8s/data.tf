30df188216a3fa421c4e9d03e817cf4ee27e937a
Paulo Barreto <monstryker@outlook.com>
2025-05-27 16:54:43 -0300

    Teste pipe

--- stacks/terraform/aws/envs/dev/k8s/data.tf
+++ stacks/terraform/aws/envs/dev/k8s/data.tf
@@ -2,11 +2,11 @@ data "aws_eks_cluster_auth" "auth" {
   name = var.cluster_name
 }
 
-# data "terraform_remote_state" "vpc" {
-#   backend = "s3"
-#   config = {
-#     bucket = "poc-multi-cloud-tfstate"
-#     key    = "vpc/stacks-terraform.tfstate"
-#     region = "us-east-1"
-#   }
-# }
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket = "poc-multi-cloud-tfstate"
+    key    = "vpc/stacks-terraform.tfstate"
+    region = "us-east-1"
+  }
+}
--- stacks/terraform/aws/envs/dev/k8s/main.tf
+++ stacks/terraform/aws/envs/dev/k8s/main.tf
@@ -30,9 +30,9 @@ module "eks" {
     }
   }
 
-  vpc_id                   = var.vpc_id
-  subnet_ids               = var.private_subnets
-  control_plane_subnet_ids = var.intra_subnets
+  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
+  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
+  control_plane_subnet_ids = data.terraform_remote_state.vpc.outputs.intra_subnets
 
   eks_managed_node_groups = {
     karpenter = {
