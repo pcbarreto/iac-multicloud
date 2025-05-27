5fde4bc98de922b0445b007a739e13dc949ddf01
Paulo Barreto <monstryker@outlook.com>
2025-05-27 16:18:09 -0300

    Teste pipe

--- stacks/terraform/aws/envs/dev/k8s/main.tf
+++ stacks/terraform/aws/envs/dev/k8s/main.tf
@@ -30,9 +30,9 @@ module "eks" {
     }
   }
 
-  vpc_id                   = data.terraform_remote_state.vpc.outputs.vpc_id
-  subnet_ids               = data.terraform_remote_state.vpc.outputs.private_subnets
-  control_plane_subnet_ids = data.terraform_remote_state.vpc.outputs.intra_subnets
+  vpc_id                   = var.vpc_id
+  subnet_ids               = var.private_subnets
+  control_plane_subnet_ids = var.intra_subnets
 
   eks_managed_node_groups = {
     karpenter = {
@@ -51,6 +51,7 @@ module "eks" {
         },
       }
       labels = {
+        # Used to ensure Karpenter runs on nodes that it does not manage
         "karpenter.sh/controller" = "true"
       }
     }
