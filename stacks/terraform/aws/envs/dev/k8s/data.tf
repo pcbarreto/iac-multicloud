0a40d5c7d42be8331965402e0154fb82a55df003
Paulo Barreto <monstryker@outlook.com>
2025-05-27 17:11:43 -0300

    Teste pipe

--- stacks/terraform/aws/envs/dev/k8s/data.tf
+++ stacks/terraform/aws/envs/dev/k8s/data.tf
@@ -3,10 +3,13 @@ data "aws_eks_cluster_auth" "auth" {
 }
 
 data "terraform_remote_state" "vpc" {
-  backend = "s3"
+  backend = "remote"
   config = {
     bucket = "poc-multicloud-tfstate"
     key    = "vpc/stacks-terraform.tfstate"
     region = "us-east-1"
+    workspaces = {
+      name = "dev"
+    }
   }
 }
