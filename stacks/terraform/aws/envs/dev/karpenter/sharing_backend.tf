007fc4f1624b2a3591ddbcea7039a78d09d8d542
Paulo Barreto <monstryker@outlook.com>
2025-05-27 17:52:43 -0300

    Teste pipe

---
+++ stacks/terraform/aws/envs/dev/karpenter/sharing_backend.tf
@@ -0,0 +1,14 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+variable "cluster_name" {
+  type = any
+}
+variable "cluster_endpoint" {
+  type = any
+}
+variable "cluster_certificate_authority_data" {
+  type = any
+}
+variable "ami_type" {
+  type = any
+}
