fc84a9ed7ea6737bdbcbef3ff439483fa334ba1d
Paulo Barreto <monstryker@outlook.com>
2025-05-27 23:19:18 -0300

    Teste Stack

--- stacks/terraform/aws/envs/dev/resources/data.tf
+++ stacks/terraform/aws/envs/dev/resources/data.tf
@@ -4,10 +4,6 @@ data "aws_eks_cluster_auth" "auth" {
 data "aws_eks_cluster" "eks" {
   name = var.cluster_name
 }
-data "aws_eks_cluster_auth" "auth" {
-  name       = var.cluster_name
-}
-
 data "terraform_remote_state" "eks" {
   backend = "s3"
   config = {
