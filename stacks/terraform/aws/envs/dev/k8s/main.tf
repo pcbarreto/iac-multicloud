d2c2d07447db9b5e083e618c269f656fdd781a38
Paulo Barreto <monstryker@outlook.com>
2025-05-30 12:20:55 -0300

    Change Eks to Latest

--- stacks/terraform/aws/envs/dev/k8s/main.tf
+++ stacks/terraform/aws/envs/dev/k8s/main.tf
@@ -149,7 +149,7 @@ resource "kubectl_manifest" "karpenter_node_class" {
 apiVersion: karpenter.k8s.aws/v1
 kind: EC2NodeClass
 metadata:
-  name: karpenter-default
+  name: karpenter
 spec:
   role: "${module.karpenter.iam_role_name}"
   amiFamily: AL2023
@@ -178,44 +178,44 @@ YAML
 
 resource "kubectl_manifest" "karpenter_node_pool" {
   yaml_body = <<-YAML
-apiVersion: karpenter.sh/v1
-kind: NodePool
-metadata:
-name: karpenter-default
-spec:
-template:
-  spec:
-    requirements:
-      - key: "karpenter.k8s.aws/instance-family"
-        operator: In
-        values: [ "t2", "t3", "t3a", "t4g"]
-      - key: "karpenter.k8s.aws/instance-size"
-        operator: In
-        values: ["small","medium"]
-      - key: kubernetes.io/os
-        operator: In
-        values: ["linux"]
-      - key: "karpenter.k8s.aws/instance-cpu"
-        operator: In
-        values: ["1","2","4","8"]
-      - key: "kubernetes.io/arch"
-        operator: In
-        values: ["amd64"]
-      - key: "karpenter.sh/capacity-type"
-        operator: In
-        values: ["on-demand"]
-    nodeClassRef:
-      group: karpenter.k8s.aws
-      kind: EC2NodeClass
-      name: karpenter-default
-    expireAfter: 720h # 30 * 24h = 720h
-limits:
-  cpu: 1000
-  memory: 1000Gi
-disruption:
-  consolidationPolicy: WhenEmptyOrUnderutilized
-  consolidateAfter: 1m
-YAML
+    apiVersion: karpenter.sh/v1
+    kind: NodePool
+    metadata:
+      name: karpenter
+    spec:
+      template:
+        spec:
+          requirements:
+            - key: "karpenter.k8s.aws/instance-family"
+              operator: In
+              values: [ "t2", "t3", "t3a","m5"]
+            - key: "karpenter.k8s.aws/instance-size"
+              operator: In
+              values: ["small","medium"]
+            - key: kubernetes.io/os
+              operator: In
+              values: ["linux"]
+            - key: "karpenter.k8s.aws/instance-cpu"
+              operator: In
+              values: ["1","2","4","8"]
+            - key: "kubernetes.io/arch"
+              operator: In
+              values: ["amd64"]
+            - key: "karpenter.sh/capacity-type"
+              operator: In
+              values: ["on-demand"]
+          nodeClassRef:
+            group: karpenter.k8s.aws
+            kind: EC2NodeClass
+            name: karpenter-default
+          expireAfter: 720h # 30 * 24h = 720h
+      limits:
+        cpu: 1000
+        memory: 1000Gi
+      disruption:
+        consolidationPolicy: WhenEmptyOrUnderutilized
+        consolidateAfter: 1m
+  YAML
 
   depends_on = [
     kubectl_manifest.karpenter_node_class
