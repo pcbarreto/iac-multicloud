8f83659d2a8d8e35248e37bf4e880a6994c427f2
Paulo Barreto <monstryker@outlook.com>
2025-05-30 21:44:12 -0300

    Change Node Groups

--- stacks/terraform/aws/envs/dev/resources/backend.tf
+++ stacks/terraform/aws/envs/dev/resources/backend.tf
@@ -1,7 +1,9 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
 terraform {
   backend "s3" {
     bucket       = "poc-multicloud-tfstate"
-    key          = "resources/stacks-terraform.tfstate"
+    key          = "resources/terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
   }
