a54ac8140d0e8679a5139ba37eaa3364f31202bc
Paulo Barreto <monstryker@outlook.com>
2025-06-02 14:16:57 -0300

    Test tfdocs

--- stacks/terraform/aws/envs/prod/karpenter/variables.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  azs = slice(data.aws_availability_zones.available.names, 0, 3)
-  tags = {
-    ManagedBy = "Terraform"
-    Owner     = "Pcbarreto"
-  }
-}
--- stacks/terraform/aws/envs/prod/monitoring/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "cluster_name" {
-  type = string
-}
-variable "oidc_role_arn" {
-  type = string
-}
-variable "kubeconfig_content" {
-  type = string
-}
