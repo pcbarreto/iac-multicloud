5202090464211d2a22592515a56ada024f6753ce
Paulo Barreto <monstryker@outlook.com>
2025-06-03 01:30:29 -0300

    end to end previwer

---
+++ stacks/terraform/aws/envs/dev/monitoring/locals.tf
@@ -0,0 +1,11 @@
+// TERRAMATE: GENERATED AUTOMATICALLY DO NOT EDIT
+
+locals {
+  tags = {
+    Environment = "dev"
+    GithubOrg   = "PcBarreto"
+    GithubRepo  = "iac-multicloud"
+    ManagedBy   = "Terraform"
+    Owner       = "Platform Engineering"
+  }
+}
