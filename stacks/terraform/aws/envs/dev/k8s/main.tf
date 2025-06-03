3df5099793308250b2f25f8568866b4206707d0a
Paulo Barreto <monstryker@outlook.com>
2025-06-03 02:47:24 -0300

    end to end previwer

--- stacks/terraform/aws/envs/dev/k8s/main.tf
+++ stacks/terraform/aws/envs/dev/k8s/main.tf
@@ -89,6 +89,27 @@ resource "aws_iam_policy" "cluster_autoscaler" {
         ],
         Resource = "*"
       }
+    ],
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateVolume",
+          "ec2:AttachVolume",
+          "ec2:DeleteVolume",
+          "ec2:DetachVolume",
+          "ec2:DescribeVolumeAttribute",
+          "ec2:DescribeVolumeStatus",
+          "ec2:DescribeVolumes",
+          "ec2:CreateSnapshot",
+          "ec2:DeleteSnapshot",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:CreateTags",
+          "ec2:DeleteTags"
+        ],
+        Resource = "*"
+      }
     ]
   })
 }
