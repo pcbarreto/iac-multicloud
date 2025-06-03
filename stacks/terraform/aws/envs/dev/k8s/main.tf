01c4d9516e76f4c06cf354600c4e008a4e7454b7
Paulo Barreto <monstryker@outlook.com>
2025-06-03 02:36:38 -0300

    end to end previwer

--- stacks/terraform/aws/envs/dev/k8s/main.tf
+++ stacks/terraform/aws/envs/dev/k8s/main.tf
@@ -45,9 +45,9 @@ module "eks" {
 
     }
 
-    iam_role_additional_policies = {
-      AmazonEBSCSIDriverPolicy  = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    }
+  }
+  iam_role_additional_policies = {
+    AmazonEBSCSIDriverPolicy  = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
   }
 
   tags = local.tags
