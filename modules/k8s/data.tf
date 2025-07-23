9a15681624b01023185153bbb4d7d32bcdf1a240
pcbarreto <paulo.barreto@vtal.com>
2025-07-23 13:02:51 -0300

    add network data

--- modules/k8s/data.tf
+++ modules/k8s/data.tf
@@ -1,6 +1,6 @@
-data "aws_eks_cluster" "cluster" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = var.cluster_name
+# }
 
 data "terraform_remote_state" "network" {
   backend = "s3"
