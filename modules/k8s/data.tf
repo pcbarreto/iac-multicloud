96e040facd47214a06a8d5f6935d976dddf9ec20
pcbarreto <paulo.barreto@vtal.com>
2025-07-23 13:01:58 -0300

    add network data

--- modules/k8s/data.tf
+++ modules/k8s/data.tf
@@ -1,7 +1,3 @@
-data "aws_eks_cluster_auth" "auth" {
-  name = var.cluster_name
-}
-
 data "aws_eks_cluster" "cluster" {
   name = var.cluster_name
 }
--- stacks/aws/envs/dev/k8s/outputs.tf
+++ stacks/aws/envs/dev/k8s/outputs.tf
@@ -7,3 +7,13 @@ output "cluster_name" {
   description = "The name of the EKS cluster"
   value       = module.eks.cluster_name
 }
+
+output "cluster_endpoint" {
+  description = "The endpoint for the EKS cluster"
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  description = "The base64 encoded certificate data required to communicate with the EKS cluster"
+  value = module.eks.cluster_certificate_authority_data
+}
